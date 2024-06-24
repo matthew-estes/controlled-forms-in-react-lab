93b17b47938ac64064fe2d93fa977951809a8288
matthew-estes <matthew.estes19@gmail.com>
2024-06-24 13:15:01 -0400

    create handleInputChange event handler

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -7,6 +7,10 @@ const [books, setBooks] = useState([
 
 const [newBook, setNewBook] = useState([{ title: "", author: "" }]);
 
+function handleInputChange(event) {
+  setNewBook({ ...newBook, [event.target.name]: event.target.value });
+}
+
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
