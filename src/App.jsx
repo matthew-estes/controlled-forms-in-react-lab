84a6a10e64ec51eabfa92a3cf205a653a7a3dadc
matthew-estes <matthew.estes19@gmail.com>
2024-06-24 11:36:03 -0400

    set up bookshelf.jsx and import into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-// src/App.jsx
 
-const App = () => {
-  return <h1>Hello world!</h1>;
+import './App.css';
+import Bookshelf from './Bookshelf.jsx';
+
+function App() {
+  
+  return (
+    <>
+      <h1>My Bookshelf</h1>
+      <Bookshelf />
+    </>
+  );
 };
 
 export default App;
